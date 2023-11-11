697725ce82478682f4018e19d2a5419362646ec5
Zakaria Ech-chifaouy <machinelearning1.618@gmail.com>
2023-11-11 21:47:21 +0100

    1337

--- ft_printf.c
+++ ft_printf.c
@@ -6,24 +6,45 @@
 /*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 21:03:04 by zech-chi          #+#    #+#             */
-/*   Updated: 2023/11/11 19:57:42 by zech-chi         ###   ########.fr       */
+/*   Updated: 2023/11/11 21:40:54 by zech-chi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-// va_arg(args, type ) type = int or char or char * ...
+void	ft_apply_specifier(char sp, int *counter, va_list args)
+{
+	if (sp == '%')
+		ft_putchar('%', counter);
+	else if (sp == 'c')
+		ft_putchar((char)va_arg(args, int), counter);
+	else if (sp == 's')
+		ft_putstr(va_arg(args, char *), counter);
+	else if (sp == 'p')
+	{
+		ft_putstr("0x", counter);
+		ft_convert(va_arg(args, unsigned long), 16, "0123456789abcdef", counter);
+	}
+	else if (sp == 'd' || sp == 'i')
+		ft_print_d_i(va_arg(args, int), counter);
+	else if (sp == 'u')
+		ft_convert(va_arg(args, unsigned int), 10, "0123456789", counter);
+	else if (sp == 'x')
+		ft_convert(va_arg(args, unsigned int), 16, "0123456789abcdef", counter);
+	else if (sp == 'X')
+		ft_convert(va_arg(args, unsigned int), 16, "0123456789ABCDEF", counter);
+}
 
-int	ft_printf(const char * str, ...)
+int	ft_printf(const char *str, ...)
 {
 	size_t	i;
-	int	counter;
+	int		counter;
 	va_list	args;
-	va_start(args, str);
 
+	va_start(args, str);
 	counter = 0;
-	i = 0;	
-	if(write(1, "", 0) == -1)
+	i = 0;
+	if (write(1, "", 0) == -1)
 		return (-1);
 	while (str[i])
 	{
@@ -32,29 +53,8 @@ int	ft_printf(const char * str, ...)
 		else
 		{
 			i++;
-			if (str[i] == '\0')
-				break;
-			else if (str[i] == '%')
-				ft_putchar('%', &counter);
-			else if (str[i] == 'c')
-				ft_putchar((char)va_arg(args, int), &counter);
-			else if (str[i] == 's')
-				ft_putstr(va_arg(args, char *), &counter);
-			else if (str[i] == 'p')
-			{
-				ft_putstr("0x", &counter);
-				ft_convert(va_arg(args, unsigned long), 16, "0123456789abcdef", &counter);
-			}
-			else if (str[i] == 'd')
-				ft_print_d_i(va_arg(args, int), &counter);
-			else if (str[i] == 'i')
-				ft_print_d_i(va_arg(args, int), &counter);
-			else if (str[i] == 'u')
-				ft_convert(va_arg(args, unsigned int), 10, "0123456789", &counter);
-			else if (str[i] == 'x')
-				ft_convert(va_arg(args, unsigned int), 16, "0123456789abcdef", &counter);
-			else if (str[i] == 'X')
-				ft_convert(va_arg(args, unsigned int), 16, "0123456789ABCDEF", &counter);
+			if (str[i] != '\0')
+				ft_apply_specifier(str[i], &counter, args);
 		}
 		if (str[i])
 			i++;
